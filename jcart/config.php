c54f55d34e0b555f5ff223053004da90ee96ebc3
michaelpitz <michael.pitz@cf2m.be>
2015-08-26 16:09:19 +0200

    config de base

--- jcart/config.php
+++ jcart/config.php
@@ -1,63 +1,55 @@
 <?php
 
-// jCart v1.3
-// http://conceptlogic.com/jcart/
-
-// Do NOT store any sensitive info in this file!!!
-// It's loaded into the browser as plain text via Ajax
+// monpanier
 
 
-////////////////////////////////////////////////////////////////////////////////
-// REQUIRED SETTINGS
-
-// Path to your jcart files
 $config['jcartPath']              = 'jcart/';
 
 // Path to your checkout page
 $config['checkoutPath']           = 'checkout.php';
 
 // The HTML name attributes used in your item forms
-$config['item']['id']             = 'my-item-id';    // Item id
-$config['item']['name']           = 'my-item-name';    // Item name
+$config['item']['id']             = 'idproduit';    // Item id
+$config['item']['name']           = 'nomproduit';    // Item name
 $config['item']['price']          = 'my-item-price';    // Item price
 $config['item']['qty']            = 'my-item-qty';    // Item quantity
 $config['item']['url']            = 'my-item-url';    // Item URL (optional)
 $config['item']['add']            = 'my-add-button';    // Add to cart button
 
-// Your PayPal secure merchant ID
+// ID du marchant Paypal
 // Found here: https://www.paypal.com/webapps/customerprofile/summary.view
-$config['paypal']['id']           = 'seller_1282188508_biz@conceptlogic.com';
+$config['paypal']['id']           = 'K7XDJ7837U33Q';
 
 ////////////////////////////////////////////////////////////////////////////////
 // OPTIONAL SETTINGS
 
 // Three-letter currency code, defaults to USD if empty
 // See available options here: http://j.mp/agNsTx
-$config['currencyCode']           = '';
+$config['currencyCode']           = 'EUR';
 
 // Add a unique token to form posts to prevent CSRF exploits
 // Learn more: http://conceptlogic.com/jcart/security.php
-$config['csrfToken']              = false;
+$config['csrfToken']              = true;
 
 // Override default cart text
-$config['text']['cartTitle']      = '';    // Shopping Cart
-$config['text']['singleItem']     = '';    // Item
-$config['text']['multipleItems']  = '';    // Items
-$config['text']['subtotal']       = '';    // Subtotal
-$config['text']['update']         = '';    // update
-$config['text']['checkout']       = '';    // checkout
-$config['text']['checkoutPaypal'] = '';    // Checkout with PayPal
-$config['text']['removeLink']     = '';    // remove
-$config['text']['emptyButton']    = '';    // empty
-$config['text']['emptyMessage']   = '';    // Your cart is empty!
-$config['text']['itemAdded']      = '';    // Item added!
-$config['text']['priceError']     = '';    // Invalid price format!
-$config['text']['quantityError']  = '';    // Item quantities must be whole numbers!
-$config['text']['checkoutError']  = '';    // Your order could not be processed!
+$config['text']['cartTitle']      = 'MonPanier';    // Shopping Cart
+$config['text']['singleItem']     = 'Produit';    // Item
+$config['text']['multipleItems']  = 'Produits';    // Items
+$config['text']['subtotal']       = 'sous-total';    // Subtotal
+$config['text']['update']         = 'Mettre à jour';    // update
+$config['text']['checkout']       = 'Payer !';    // checkout
+$config['text']['checkoutPaypal'] = 'Payer avec Paypal!';    // Checkout with PayPal
+$config['text']['removeLink']     = 'Supprimer';    // remove
+$config['text']['emptyButton']    = 'Vider';    // empty
+$config['text']['emptyMessage']   = 'Votre panier est vide!';    // Your cart is empty!
+$config['text']['itemAdded']      = 'Produit ajouté!';    // Item added!
+$config['text']['priceError']     = 'Format de prix invalide';    // Invalid price format!
+$config['text']['quantityError']  = 'Problème de quantité';    // Item quantities must be whole numbers!
+$config['text']['checkoutError']  = 'Problème lors de la finalisation de l\'achat';    // Your order could not be processed!
 
 // Override the default buttons by entering paths to your button images
-$config['button']['checkout']     = '';
-$config['button']['paypal']       = '';
+$config['button']['checkout']     = ''; 
+$config['button']['paypal']       = ''; 
 $config['button']['update']       = '';
 $config['button']['empty']        = '';
 
@@ -75,7 +67,7 @@ $config['decimalQtys']            = false;
 $config['decimalPlaces']          = 1;
 
 // Number format for prices, see: http://php.net/manual/en/function.number-format.php
-$config['priceFormat']            = array('decimals' => 2, 'dec_point' => '.', 'thousands_sep' => ',');
+$config['priceFormat']            = array('decimals' => 2, 'dec_point' => ',', 'thousands_sep' => '.');
 
 // Send visitor to PayPal via HTTPS?
 $config['paypal']['https']        = true;
